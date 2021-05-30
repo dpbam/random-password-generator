92a8fdbb0a82cc091e365b70289959e22e0a0b29
Derek Phelps <derekphtpt@gmail.com>
2021-05-29 22:04:13 -0600

    updated readME

--- scrap.js
+++
@@ -1,9 +0,0 @@
-var lowerChars = 'abcdefghijklmnopqrstuvwxyz0123456789';
-var result = ""
-var chaactersLength = characters.length;
-
-for ( var i = 0; i < 5 ; i++ ) {
-    result += lowerChars.charAt(Math.floor(Math.random() * chaactersLength));
-}
-
-console.log(result)
